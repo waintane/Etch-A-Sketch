ee8f1ce3eac941878f6bd4d06283e2c792897113
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-05-14 15:24:08 -0400

    construction du cadrie via le Javascript

---
+++ javascript/javascript.js
@@ -0,0 +1,26 @@
+function spawnGrid(){
+    for(i=0 ; i <= 16; i++){
+
+        let container = document.createElement("div");
+        container.classList.add("column");
+        document.querySelector("body").appendChild(container);
+
+        
+    }
+
+    let containers = document.querySelectorAll(".column");
+
+    containers.forEach(function(e){
+
+        for (i=0; i<=16; i++){
+            let div =document.createElement("div");
+            div.classList.add("cadrier")
+            e.appendChild(div);
+        }
+
+    })
+    
+    console.log(containers);
+}
+
+spawnGrid();
