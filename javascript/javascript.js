fa7092adfbeb0f945cc473541a710bebbf2d1e3f
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-05-15 11:25:10 -0400

    ajout de la gestion de dimension de la grille

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,9 @@
+let spawnGridValidateur = true;
+let valeurBase = 16
 //gestion de la grid
-function spawnGrid(dimension){
+function spawnGrid(v = 16){
     //faire apparaitre les containers verticale
-    for(i=0 ; i <= 16; i++){
+    for(i=0 ; i <= v; i++){
 
         let container = document.createElement("div");
         container.classList.add("column");
@@ -15,23 +17,38 @@ function spawnGrid(dimension){
     //faire apparaitre toute les cases
     containers.forEach(function(e){
 
-        for (i=0; i<=16; i++){
+        for (i=0; i<=v; i++){
             let div =document.createElement("div");
             div.classList.add("cadrier")
             e.appendChild(div);
         }
 
     })
-    
+    valeurBase = v;
+    spawnGridValidateur = false;
     console.log(containers);
 }
 
 let boutonDimension = document.querySelector(".boutonDimension");
 
 boutonDimension.addEventListener("click", () => {
-    let value = prompt("quel dimension souhaitez-vous?");
+    let value = prompt("quel dimension souhaitez-vous? 100 étant la limite");
 
-    
+    if (value > 100){
+        value = 100;
+    }
+
+    console.log(value);
+
+    for (i =0 ; i <= valeurBase ; i++){
+        let grille = document.querySelector(".column");
+
+        grille.parentNode.removeChild(grille);
+    }
+
+    spawnGrid(value);
 })
 
-spawnGrid();
+if (spawnGridValidateur === true){
+    spawnGrid();
+}
